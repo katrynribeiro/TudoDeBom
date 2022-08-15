865dd7c39a863eaab25196d80f6b60810e9b5eba
katrynribeiro <kakarilber@hotmail.com>
2022-08-15 11:46:56 -0300

    "Enviando JFrame Clientes"

--- src/Sistema/TelaLogin.java
+++ src/Sistema/TelaLogin.java
@@ -19,6 +19,9 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JPasswordField;
 import javax.swing.JCheckBox;
+import javax.swing.ImageIcon;
+import java.awt.Color;
+import java.awt.Font;
 
 public class TelaLogin extends JFrame {
 
@@ -59,11 +62,15 @@ public class TelaLogin extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
-		JLabel lblNewLabel = new JLabel("Usuário");
-		lblNewLabel.setBounds(189, 26, 46, 14);
+		JLabel lblNewLabel = new JLabel("USUÁRIO");
+		lblNewLabel.setFont(new Font("Segoe UI", Font.BOLD, 11));
+		lblNewLabel.setForeground(new Color(245, 255, 250));
+		lblNewLabel.setBounds(177, 26, 59, 14);
 		contentPane.add(lblNewLabel);
 
-		JLabel lblNewLabel_1 = new JLabel("Senha");
+		JLabel lblNewLabel_1 = new JLabel("SENHA");
+		lblNewLabel_1.setForeground(new Color(240, 255, 255));
+		lblNewLabel_1.setFont(new Font("Segoe UI", Font.BOLD, 11));
 		lblNewLabel_1.setBounds(189, 92, 46, 14);
 		contentPane.add(lblNewLabel_1);
 
@@ -82,6 +89,7 @@ public class TelaLogin extends JFrame {
 		contentPane.add(btnLogin);
 
 		JLabel lblStatus = new JLabel("status");
+		lblStatus.setForeground(new Color(240, 255, 255));
 		if (conexao != null) {
 			lblStatus.setText("Banco de Dados: Conectado");
 		} else {
@@ -93,6 +101,11 @@ public class TelaLogin extends JFrame {
 		textSenha = new JPasswordField();
 		textSenha.setBounds(115, 117, 182, 20);
 		contentPane.add(textSenha);
+		
+		JLabel lblNewLabel_2 = new JLabel("New label");
+		lblNewLabel_2.setIcon(new ImageIcon(TelaLogin.class.getResource("/Imagens/d471b4f2ce7b4725607c504a1e8da094.jpg")));
+		lblNewLabel_2.setBounds(0, 0, 412, 218);
+		contentPane.add(lblNewLabel_2);
 	}
 
 	public void logar() {
@@ -100,11 +113,15 @@ public class TelaLogin extends JFrame {
 		try {
 			pst = conexao.prepareStatement(sql);
 			pst.setString(1, textUsuario.getText());
-			pst.setString(2, textSenha.getText());
+			String captura = new String(textSenha.getPassword());
+			pst.setString(2, captura);
 			rs = pst.executeQuery();
+			
 			if (rs.next()) {
 				TelaPrincipal principal = new TelaPrincipal();
 				principal.setVisible(true);
+				this.dispose();
+				conexao.close();
 			} else {
 				JOptionPane.showMessageDialog(null, "usuário e/ou senha invalido(s)");
 			}
--- src/Sistema/TelaPrincipal.java
+++ src/Sistema/TelaPrincipal.java
@@ -6,10 +6,27 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import java.awt.Font;
+import javax.swing.JDesktopPane;
+import java.awt.Color;
+import javax.swing.JLayeredPane;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JInternalFrame;
+import java.awt.Dialog.ModalExclusionType;
+import java.awt.Toolkit;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.BoxLayout;
+import javax.swing.SwingConstants;
 
 public class TelaPrincipal extends JFrame {
 
-	private JPanel contentPane;
+	private JPanel desktop;
 
 	/**
 	 * Launch the application.
@@ -31,12 +48,64 @@ public class TelaPrincipal extends JFrame {
 	 * Create the frame.
 	 */
 	public TelaPrincipal() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 542, 480);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
+		setTitle("Farmácia Tudo de Bom");
+		setIconImage(Toolkit.getDefaultToolkit().getImage(TelaPrincipal.class.getResource("/Imagens/d471b4f2ce7b4725607c504a1e8da094.jpg")));
+		setResizable(false);
+		setBounds(100, 100, 695, 524);
+		
+		JMenuBar menu = new JMenuBar();
+		setJMenuBar(menu);
+		
+		JMenu menCadastro = new JMenu("Cadastro");
+		menCadastro.setFont(new Font("Segoe UI Historic", Font.PLAIN, 13));
+		menu.add(menCadastro);
+		
+		JMenuItem menCadCli = new JMenuItem("Cliente");
+		menCadCli.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				TelaCliente telaCliente = new TelaCliente();
+				telaCliente.setVisible(true);
+				desktop.add(telaCliente);
+			}
+		});
+		menCadastro.add(menCadCli);
+		
+		JMenu menRelatorio = new JMenu("Relatório");
+		menRelatorio.setFont(new Font("Segoe UI Historic", Font.PLAIN, 13));
+		menu.add(menRelatorio);
+		
+		JMenuItem menRelEst = new JMenuItem("Estoque");
+		menRelatorio.add(menRelEst);
+		
+		JMenuItem menHistTrans = new JMenuItem("Histórico de transações");
+		menRelatorio.add(menHistTrans);
+		
+		JMenu menOpcoes = new JMenu("Opções");
+		menOpcoes.setFont(new Font("Segoe UI Historic", Font.PLAIN, 13));
+		menu.add(menOpcoes);
+		
+		JMenuItem menOpAjuda = new JMenuItem("Ajuda");
+		menOpAjuda.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		menOpcoes.add(menOpAjuda);
+		
+		JMenuItem menOpSair = new JMenuItem("Sair");
+		menOpSair.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int sair = JOptionPane.showConfirmDialog(null, "Tem certeza que deseja sair?","Atenção",JOptionPane.YES_NO_OPTION);
+			if(sair==JOptionPane.YES_OPTION) {
+				System.exit(0);
+			}
+			}
+		});
+		menOpcoes.add(menOpSair);
+		desktop = new JPanel();
+		desktop.setBackground(new Color(102, 205, 170));
+		desktop.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(desktop);
+		desktop.setLayout(null);
 	}
-
 }
